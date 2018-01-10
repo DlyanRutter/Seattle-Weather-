d9ac16839f2fe36504acc12f47119b9d3a2784a7
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2018-01-09 19:51:45 -0600

    Add files via upload

---
+++ sklearn classifiers.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier,\
+     GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from seattle_weather_helper import get_data, accuracy
+
+features, labels, one_hot = get_data()
+print np.any(np.isnan(features)) + 'is nan features'
+print np.all(np.isfinite(features)) + 'is finite features'
+print np.any(np.isnan(labels)) + 'is nan labels'
+print np.all(np.isfinite(labels)) + 'is finite labels'
+
+
+X_train, X_test = features[:20000], features[20001:]
+y_train, y_test = labels[:20000], labels[20001:]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="rbf", C=0.025, probability=True),
+    NuSVC(probability=True),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    AdaBoostClassifier(),
+    GradientBoostingClassifier(),
+    GaussianNB(),
+    LinearDiscriminantAnalysis(),
+    QuadraticDiscriminantAnalysis()]
+
+# Logging for Visual Comparison
+log_cols=["Classifier", "Accuracy", "Log Loss"]
+log = pd.DataFrame(columns=log_cols)
+
+for clf in classifiers:
+    clf.fit(X_train, y_train)
+    name = clf.__class__.__name__
+    
+    print("="*30)
+    print(name)
+    
+    print('****Results****')
+    train_predictions = clf.predict(X_test)
+    acc = accuracy_score(y_test, train_predictions)
+    print("Accuracy: {:.4%}".format(acc))
+    
+    train_predictions = clf.predict_proba(X_test)
+    ll = log_loss(y_test, train_predictions)
+    print("Log Loss: {}".format(ll))
+    
+    log_entry = pd.DataFrame([[name, acc*100, ll]], columns=log_cols)
+    log = log.append(log_entry)
+    
+print("="*30)
+
+sns.set_color_codes("muted")
+sns.barplot(x='Accuracy', y='Classifier', data=log, color="b")
+
+plt.xlabel('Accuracy %')
+plt.title('Classifier Accuracy')
+plt.show()
+
+sns.set_color_codes("muted")
+sns.barplot(x='Log Loss', y='Classifier', data=log, color="g")
+
+plt.xlabel('Log Loss')
+plt.title('Classifier Log Loss')
+plt.show()
+
+# Predict Test Set
+favorite_clf = LinearDiscriminantAnalysis()
+favorite_clf.fit(X_train, y_train)
+test_predictions = favorite_clf.predict_proba(test)
+
+# Format DataFrame
+submission = pd.DataFrame(test_predictions, columns=classes)
+submission.insert(0, 'id', test_ids)
+submission.reset_index()
+
+# Export Submission
+#submission.to_csv('submission.csv', index = False)
+submission.tail()
